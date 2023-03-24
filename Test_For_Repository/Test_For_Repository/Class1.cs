c3786c366c4cefdb3b4aabe0f5c9d42796622823
DanielSvensson22 <111700515+DanielSvensson22@users.noreply.github.com>
2023-03-24 08:58:51 +0100

    Create Class1.cs

---
+++ Test_For_Repository/Test_For_Repository/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_For_Repository
+{
+    internal class Class1
+    {
+        public static void PrintLineOne() { Console.WriteLine("This is the first feature!"); }
+    }
+}
