9011570442f336fc3ad02f3fbeea914f1819b59d
DanielSvensson22 <111700515+DanielSvensson22@users.noreply.github.com>
2023-03-24 08:55:53 +0100

    Added a Hello World! c# application

---
+++ Test_For_Repository/Test_For_Repository/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_For_Repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
