1504a9aeb8890234e0a69ec332e39c52544ebba2
DanielSvensson22 <111700515+DanielSvensson22@users.noreply.github.com>
2023-03-24 09:00:06 +0100

    Create Class2.cs

---
+++ Test_For_Repository/Test_For_Repository/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_For_Repository
+{
+    internal class Class2
+    {
+        public static void PrintFeatureTwo() { Console.WriteLine("This is the second feature!"); }
+    }
+}
